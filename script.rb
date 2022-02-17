816d9fe6506f2616927730e98df23a7415933914
supersakana <zacwilliamson@icloud.com>
2022-02-17 14:08:23 -0600

    hella complicated but gets the highest profit value

--- script.rb
+++ script.rb
@@ -1,6 +1,24 @@
 # ruby script.rb
 
 def stock_picker (stocks)
-    puts stocks
+    buy_sell = Array.new
+    best_sells = Array.new
+
+    stocks.each do |first_num, next_num|
+        i = 1
+        while i < stocks.length
+        next_num = stocks[stocks.index(first_num) + i] 
+        total = next_num.to_i - first_num.to_i
+        if total > 0
+            puts "#{next_num} - #{first_num} = #{total}"
+            best_sells.push(total)
+            end
+        i += 1
+        end
+    end
+    print best_sells
 end
-stock_picker([17,3,6,9,15,8,6,1,10])
+
+data = [17,3,6,9,15,8,6,1,10]
+stock_picker(data)
+

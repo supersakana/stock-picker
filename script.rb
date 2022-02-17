7b3766ee6247c2a04b37d7d3a5b7aa90b91117a5
supersakana <zacwilliamson@icloud.com>
2022-02-17 14:22:31 -0600

    method returns buy day

--- script.rb
+++ script.rb
@@ -1,22 +1,28 @@
 # ruby script.rb
 
 def stock_picker (stocks)
-    buy_sell = Array.new
     best_sells = Array.new
+    buy_price = Array.new
+    buy_day = Array.new
 
     stocks.each do |first_num, next_num|
         i = 1
         while i < stocks.length
+
         next_num = stocks[stocks.index(first_num) + i] 
         total = next_num.to_i - first_num.to_i
+
         if total > 0
-            puts "#{next_num} - #{first_num} = #{total}"
+        
             best_sells.push(total)
+            buy_price.push(first_num)
+
             end
         i += 1
         end
     end
-    print best_sells
+    buy_day[0] = stocks.index(buy_price[best_sells.index(best_sells.max)])
+    puts buy_day
 end
 
 data = [17,3,6,9,15,8,6,1,10]

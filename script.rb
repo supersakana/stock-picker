ccef256b0bf08e14f032b49f1a2f3713f1560c26
supersakana <zacwilliamson@icloud.com>
2022-02-27 17:10:39 -0600

    updated format with rubocop

--- script.rb
+++ script.rb
@@ -1,31 +1,30 @@
-def stock_picker (stocks)
-    best_sells = Array.new
-    buy_price = Array.new
-    sell_price = Array.new
-    buy_sell = Array.new
+def stock_picker(stocks)
+  best_sells = []
+  buy_price = []
+  sell_price = []
+  buy_sell = []
 
-    stocks.each do |first_num, next_num|
-        i = 1
-        while i < stocks.length
+  stocks.each do |first_num, next_num|
+    i = 1
+    while i < stocks.length
 
-        next_num = stocks[stocks.index(first_num) + i] 
-        total = next_num.to_i - first_num.to_i
+      next_num = stocks[stocks.index(first_num) + i]
+      total = next_num.to_i - first_num.to_i
 
-        best_sells.push(total)
-        buy_price.push(first_num)
-        sell_price.push(next_num)
-        
-        i += 1
-        end
+      best_sells.push(total)
+      buy_price.push(first_num)
+      sell_price.push(next_num)
+
+      i += 1
     end
+  end
 
-    buy_sell[0] = stocks.index(buy_price[best_sells.index(best_sells.max)])
-    buy_sell[1] = stocks.index(sell_price[best_sells.index(best_sells.max)])
-   
-    p buy_sell
-    p stocks
+  buy_sell[0] = stocks.index(buy_price[best_sells.index(best_sells.max)])
+  buy_sell[1] = stocks.index(sell_price[best_sells.index(best_sells.max)])
+
+  p buy_sell
+  p stocks
 end
 
-data = [17,3,6,9,15,8,6,1,10].shuffle
+data = [17, 3, 6, 9, 15, 8, 6, 1, 10].shuffle
 stock_picker(data)
-
